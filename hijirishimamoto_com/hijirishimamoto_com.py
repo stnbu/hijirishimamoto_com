e38b28e41b809b42f34d16da88c0981f0120f40c
Mike Burr <mburr@vmware.com>
2017-05-21 13:12:22 -0700

    separate css file

--- hijirishimamoto_com/hijirishimamoto_com.py
+++ hijirishimamoto_com/hijirishimamoto_com.py
@@ -15,35 +15,7 @@ from flask import render_template_string
 HTML = u"""<!DOCTYPE html>
 <html>
 <head>
-<style>
-ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    background-color: #333;
-}
-
-li {
-    float: left;
-}
-
-li a {
-    display: block;
-    color: white;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-}
-
-li a:hover:not(.active) {
-    background-color: #111;
-}
-
-.active {
-    background-color: #4CAF50;
-}
-</style>
+<link rel="stylesheet" type="text/css" href="/static/style.css">
 </head>
 <body>
 {{ menu|safe }}
