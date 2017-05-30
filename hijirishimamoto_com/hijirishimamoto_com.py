9676d26c800233cecdebf27533b63a2837e41fcc
Mike Burr <mburr@vmware.com>
2017-05-29 17:18:39 -0700

    do post right. sweet css

--- hijirishimamoto_com/hijirishimamoto_com.py
+++ hijirishimamoto_com/hijirishimamoto_com.py
@@ -3,11 +3,12 @@
 import os
 from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-    flash, render_template_string, abort, send_from_directory, render_template
+    flash, render_template_string, abort, send_from_directory, render_template, request
 
 
 # create our little application :)
 app = Flask(__name__)
+app.secret_key = 'ngioewsnfewldjnfv jc gnkj cf nxjkhc fnj bfsz'
 
 
 HTML = u"""<!DOCTYPE html>
@@ -46,17 +47,6 @@ class RegistrationForm(Form):
     email = StringField('Email Address', [validators.Length(min=6, max=35)])
     message = TextAreaField(u'Message', [validators.optional(), validators.length(max=200)])
 
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm(request.form)
-    if request.method == 'POST' and form.validate():
-        data = form.name.data, form.email.data, form.message.data
-        flash('Thank You. Your message has been sent.')
-        return redirect(url_for('register'))
-    return render_template('register.html', form=form)
-
-
 def get_content(page_name):
     my_dir = os.path.dirname(os.path.realpath(__file__))
     my_file = os.path.join(my_dir, 'pages', '{0}.html'.format(page_name))
@@ -87,11 +77,16 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/<page_name>/')
+@app.route('/<page_name>/', methods=['GET', 'POST'])
 def pages(page_name):
     if page_name not in PAGES:
         abort(404)
     menu = get_menu(active=page_name)
+    if request.method == 'POST':
+        if page_name != 'contact':
+            raise Exception('Not here!')
+        result = render_template_string(HTML, menu=menu, content='<div>Thank You. Your message has been sent.')
+        return result
     content = get_content(page_name)
     result = render_template_string(HTML, menu=menu, content=content)
     return result
